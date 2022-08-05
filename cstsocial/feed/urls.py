3d6b1bc7e7587c480c7d2fe9cd13c65200ca81a6
Datuna <datuna@Datunas-MacBook-Air.local>
2022-08-05 14:40:06 +0400

    update and delete post

--- cstsocial/feed/urls.py
+++ cstsocial/feed/urls.py
@@ -5,4 +5,6 @@ app_name = "feed"
 urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("post_detail/<int:id>/", views.post_detail, name="post_detail"),
+    path("delete_post/<int:id>/", views.delete_post, name="delete_post"),
+    path("update_post/<int:id>/", views.update_post, name="update_post"),
 ]
--- cstsocial/feed/views.py
+++ cstsocial/feed/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 from .forms import PostForm, CommentForm
 from .models import Post
 
@@ -27,3 +29,26 @@ def post_detail(request, id):
         comment.save()
         return redirect("users:post_detail", id=id)
     return render(request, "users/post_detail.html", {"post": post, "form": form})
+
+
+@login_required(login_url="users:login")
+def update_post(request, id):
+    post = Post.objects.get(id=id)
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect("users:post_detail", id=id)
+    return render(request, "users/update_post.html", {"form": form, "post": post})
+
+
+@login_required(login_url="users:login")
+def delete_post(request, id):
+    post = Post.objects.get(id=id)
+    if post.user == request.user:
+        post.delete()
+        messages.success(request, "Post deleted successfully")
+        return redirect("users:dashboard")
+    else:
+        messages.warning(request, "You can only delete your own posts")
+        redirect("users:post_detail", id=post.id)
+    return redirect("users:dashboard")
